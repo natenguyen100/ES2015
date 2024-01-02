dd8a6771acf5f3260fbfe3ab57b08d5421ffbef1
Nate Nguyen <nathannguyen555@gmail.com>
2024-01-01 18:02:14 -0600

    new file exercise

---
+++ letandconstexercise.js
@@ -0,0 +1,23 @@
+var PI = 3.14;
+PI = 42; // stop me from doing this!
+
+let PI = 3.14
+PI = 42
+
+Const PI = 3.14
+PI = 42
+
+// ## **Quiz**
+
+// - What is the difference between ***var*** and ***let***?
+    // var can re-assign and re-declare. You can access a hoisted variable with var.
+    // let you can not re-declare but you can re-assign
+// - What is the difference between **var** and ***const***?
+    // var can re-assign and re-declare. You can access a hoisted variable with var.
+    // const you can not re-decalure or re-assign
+// - What is the difference between **let** and **const**?
+    // let you can not re-declare but you can re-assign
+    // const you can not re-decalure or re-assign
+// - What is hoisting?
+    //a way that we explain how the JS compiler works. Variables are lifting or "hoistied" to the top of the scope they are declared in.
+    
